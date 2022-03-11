4d70d534579318f836fb2b18e345fc336f6f5701
diabaatwit <diabahmed919@gmail.com>
2022-03-11 13:13:23 -0500

    setting up the environment for server side and client side

---
+++ server/index.js
@@ -0,0 +1,16 @@
+// server/index.js
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello Amanda!" });
+  });
+ 
