f42618aad3288bcc59a78117af0c8892e1f0c434
diabaatwit <diabahmed919@gmail.com>
2022-03-18 02:24:48 -0400

    made the back-end get the jobs data from adzuna api depends on the search and location that is entered in the url

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    APP_ID: "5daf629b",
+    API_KEY: "33a9c06a987c79f32476edcf649d4d85",
+    BASE_URL: 'https://api.adzuna.com/v1/api/jobs',
+    BASE_PARAMS: 'search/1?&results_per_page=20&content-type=application/json',
+  };
--- server/index.js
+++ server/index.js
@@ -8,11 +8,22 @@ const app = express();
 
 const mongoose = require('mongoose')
 
+const axios = require('axios');
+const inputURL = require('url');
+const router = express.Router()
+const config = require('./config');
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET',
+};
+
 // database url.
 const url = 'mongodb+srv://nocturnals:AeT3RFlq38I3BXWp@cluster0.fqifx.mongodb.net/remoteJobDB?retryWrites=true&w=majority'
 
 // connecting to the database
-mongoose.connect(url, { useNewUrlParser: true})
+mongoose.connect(url, { useNewUrlParser: true })
 
 // check if the connection was successful or no
 const db = mongoose.connection
@@ -25,16 +36,41 @@ app.listen(PORT, () => {
 
 
 app.get("/", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
- 
-
-// telling the app that we'll use json
-app.use(express.json())
+  res.json({ message: "Hello from server!" });
+});
 
 // use accounts router in http://localhost:3001/accounts
 const accountRouter = require('./routes/accountRestApi')
 app.use('/accounts', accountRouter)
 app.get("/accounts", (req, res) => {
-    res.json({ message: "accounts server" });
-  });
+  res.json({ message: "accounts server" });
+});
+
+app.get("/jobs", (req, res) => {
+  const requestURL = inputURL.parse(req.url);
+  const decodedParams = decodeParams(new URLSearchParams(requestURL.search));
+  const { search, location, country = 'us' } = decodedParams;
+
+  const targetURL = `${config.BASE_URL}/${country.toLowerCase()}/${config.BASE_PARAMS}&app_id=${config.APP_ID}&app_key=${config.API_KEY}&what=${search}&where=${location}`;
+  if (req.method === 'GET') {
+    console.log(`Proxy GET request to : ${targetURL}`);
+    axios.get(targetURL)
+      .then(response => {
+        res.writeHead(200, headers);
+        res.end(JSON.stringify(response.data));
+      })
+      .catch(response => {
+        console.log(response);
+        res.writeHead(500, headers);
+        res.end(JSON.stringify(response));
+      });
+  }
+})
+
+
+// telling the app that we'll use json
+app.use(express.json())
+
+const decodeParams = searchParams => Array
+  .from(searchParams.keys())
+  .reduce((acc, key) => ({ ...acc, [key]: searchParams.get(key) }), {});
