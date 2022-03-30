d3566ea5f27c8839e1e21bebe54d707d2d8c5332
diabaatwit <diabahmed919@gmail.com>
2022-03-30 04:43:48 -0400

    backend: added get one job, and delete job restapi to handle get and delete job requests. Frontend: in saved jobs page, added if no saved jobs yet, display no saved jobs message. otherwise, display jobs

--- server/routes/jobRestApi.js
+++ server/routes/jobRestApi.js
@@ -6,14 +6,14 @@ const cors = require('cors')
 //Give write access to server
 const whitelist = ["http://localhost:3000"]
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
+    origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
+    credentials: true,
 }
 router.use(cors(corsOptions))
 
@@ -23,36 +23,74 @@ const Job = require('../models/job')
 
 // Getting all jobs
 router.get('/', async (req, res) => {
-  try {
-    // get all jobs, and retrieve it in json format.
-    const jobs = await Job.find()
+    try {
+        // get all jobs, and retrieve it in json format.
+        const jobs = await Job.find()
+        res.header("Access-Control-Allow-Origin", "*")
+        res.json(jobs)
+    }
+    catch (err) {
+        // if error, display error 500
+        res.status(500).json({ message: err.message })
+    }
+})
+
+// getting one job
+router.get('/:id', getJob, (req, res) => {
     res.header("Access-Control-Allow-Origin", "*")
-    res.json(jobs)
-  }
-  catch (err) {
-    // if error, display error 500
-    res.status(500).json({ message: err.message })
-  }
+    res.json(res.job)
 })
 
 // adding a job
 router.post('/', async (req, res) => {
     const job = new Job({
-      title: req.body.title,
-      location: req.body.location,
-      description: req.body.description,
-    }) 
-    try {   
-      // add the job
-      const newJob = await job.save()
-      res.header("Access-Control-Allow-Origin", "*")
-      console.log(newJob)
-      res.status(201).json(newJob)
+        title: req.body.title,
+        location: req.body.location,
+        description: req.body.description,
+    })
+    try {
+        // add the job
+        const newJob = await job.save()
+        res.header("Access-Control-Allow-Origin", "*")
+        console.log(newJob)
+        res.status(201).json(newJob)
     } catch (err) {
-      // if error, display error 400
-      res.status(400).json({ message: err.message })
+        // if error, display error 400
+        res.status(400).json({ message: err.message })
     }
-  });
+});
+
+// deleting a job
+router.delete('/:id', getJob, async (req, res) => {
+    try {
+        // delete job with this id.
+        await res.job[0].remove()
+        res.json({ message: 'Job deleted' })
+    } catch (err) {
+        // if error, display error 500
+        res.status(500).json({ message: err.message })
+    }
+})
+
+
+// getting a specific job by id
+async function getJob(req, res, next) {
+    let job = []
+    try {
+        // get job by id.
+        job[0] = await Job.findById(req.params.id)
+        // if there is no job with this id, display cannot find job.
+        if (job == null) {
+            return res.status(404).json({ message: 'Cannot find job' })
+        }
+    } catch (err) {
+        // if error, display error 500
+        return res.status(500).json({ message: err.message })
+    }
+
+    res.job = job
+    next()
+}
 
 
 module.exports = router
