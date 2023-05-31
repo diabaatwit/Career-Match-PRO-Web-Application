598c13c583ba0b23ea86b43639a52391141e6abd
diabaatwit <diabahmed919@gmail.com>
2023-05-30 22:05:05 -0400

    fix double scrollbars issue, and made the jobInfo section scrollable when needed

--- client/src/components/jobCard.js
+++ client/src/components/jobCard.js
@@ -3,165 +3,162 @@ import '../css/searchJob.css'
 import { ThreeDots } from 'react-loader-spinner';
 
 class JobCard extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            results: [],
-            saveText: "Save Job",
-            isLoading: false,
-            selectedJobIndex: null
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      results: [],
+      saveText: "Save Job",
+      isLoading: false,
+      selectedJobIndex: null
+    }
+  }
+
+  componentDidMount() {
+    this.fetchingJobs()
+  }
+
+  async saveJob(jobTitle, jobLocation, jobDescription, organizationName, salary, jobBoard, url) {
+    const newSavedJob = {
+      jobTitle: jobTitle,
+      jobLocation: jobLocation,
+      jobDescription: jobDescription,
+      organizationName: organizationName,
+      salary: salary,
+      jobBoard: jobBoard,
+      url: url,
+      accountID: localStorage.getItem('userID'),
     }
 
-    componentDidMount() {
-        this.fetchingJobs()
+    const newSavedJobOptions = {
+
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newSavedJob)
+
     }
 
-    async saveJob(jobTitle, jobLocation, jobDescription, organizationName, salary, jobBoard, url) {
-        const newSavedJob = {
-            jobTitle: jobTitle,
-            jobLocation: jobLocation,
-            jobDescription: jobDescription,
-            organizationName: organizationName,
-            salary: salary,
-            jobBoard: jobBoard,
-            url: url,
-            accountID: localStorage.getItem('userID'),
-        }
+    await fetch("http://localhost:3001/savedJobs", newSavedJobOptions)
+      .then(response => response.text())
+      .catch(error => console.log('error', error));
+  }
 
-        const newSavedJobOptions = {
+  changeText = (id, saveText) => {
+    console.log("id=" + id);
 
-            method: "POST",
-            mode: "cors",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(newSavedJob)
+    this.setState({ saveText });
+  }
 
-        }
+  // authorize user's input
+  async fetchingJobs() {
+    this.setState({ isLoading: true })
+    // fetch jobs
+    let url = 'http://localhost:3001/jobs/?search=' + localStorage.getItem("jobTitle") + '&location=' + localStorage.getItem("location")
+    console.log(url)
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Request failed');
+      }
 
-        await fetch("http://localhost:3001/savedJobs", newSavedJobOptions)
-            .then(response => response.text())
-            .catch(error => console.log('error', error));
+      const results = await response.json();
+      console.log(results);
+      this.setState({ results: results }, () => {
+        console.log('Results are:');
+        console.log(this.state.results);
+        console.log(localStorage.getItem('userID'));
+        this.setState({ isLoading: false }, () => {
+          console.log(this.state.isLoading);
+        });
+      });
+    } catch (error) {
+      console.log('Error:', error);
+      this.setState({ isLoading: false });
     }
+  }
 
-    changeText = (id, saveText) => {
-        console.log("id=" + id);
+  toggleJobInfo = jobIndex => {
+    this.setState(prevState => ({
+      selectedJobIndex: prevState.selectedJobIndex === jobIndex ? null : jobIndex
+    }));
+  };
 
-        this.setState({ saveText });
-    }
+  jobInfo = () => {
+    console.log("here we rendered the job info card")
+    return (
+      <div class="jobInfo_body">
+        <h4>Job Information</h4>
+        <p>This is a test message for jobInfo function.</p>
+      </div>
+    )
+  }
 
-    // authorize user's input
-    async fetchingJobs() {
-        this.setState({ isLoading: true })
-        // fetch jobs
-        let url = 'http://localhost:3001/jobs/?search=' + localStorage.getItem("jobTitle") + '&location=' + localStorage.getItem("location")
-        console.log(url)
-        try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error('Request failed');
-            }
-
-            const results = await response.json();
-            console.log(results);
-            this.setState({ results: results }, () => {
-                console.log('Results are:');
-                console.log(this.state.results);
-                console.log(localStorage.getItem('userID'));
-                this.setState({ isLoading: false }, () => {
-                    console.log(this.state.isLoading);
-                });
-            });
-        } catch (error) {
-            console.log('Error:', error);
-            this.setState({ isLoading: false });
-        }
-    }
-
-    toggleJobInfo = jobIndex => {
-        this.setState(prevState => ({
-          selectedJobIndex: prevState.selectedJobIndex === jobIndex ? null : jobIndex
-        }));
-      };
-
-    jobInfo = () => {
-        console.log("here we rendered the job info card")
-        return (
-            <div class="jobInfo_body">
-                <h4>Job Information</h4>
-                <p>This is a test message for jobInfo function.</p>
+  render() {
+    console.log("we are here in the job card component")
+    return (
+      <div className="container">
+        <div className="jobCardsContainer">
+          {this.state.isLoading ? (
+            <div className="loader">
+              <ThreeDots type="ThreeDots" color="#BADA55" height={100} width={100} />
             </div>
-        )
-    }
-
-    render() {
-        console.log("we are here in the job card component")
-        return (
-            <div className="container">
-              <div className="jobCardsContainer">
-                {this.state.isLoading ? (
-                  <div className="loader">
-                    <ThreeDots type="ThreeDots" color="#BADA55" height={100} width={100} />
+          ) : (
+            this.state.results.map((job, index) => {
+              const isJobSelected = this.state.selectedJobIndex === index;
+              return (
+                <div key={index} className={`card${isJobSelected ? " selected" : ""}`}>
+                  <div className="card-body" onClick={() => this.toggleJobInfo(index)}>
+                    <h4 className="card-title">{job.jobTitle} - {job.jobBoard}</h4>
+                    <h5 className="card-title">{job.organizationName}</h5>
+                    <h6>{job.jobLocation}</h6>
+                    <h6>{job.salary}</h6>
+                    
                   </div>
-                ) : (
-                  this.state.results.map((job, index) => {
-                    const isJobSelected = this.state.selectedJobIndex === index;
-                    return (
-                      <div key={index} className={`card${isJobSelected ? " selected" : ""}`}>
-                        <div className="card-body" onClick={() => this.toggleJobInfo(index)}>
-                          <h4 className="card-title">{job.jobTitle} - {job.jobBoard}</h4>
-                          <h5 className="card-title">{job.organizationName}</h5>
-                          <h6>{job.jobLocation}</h6>
-                          <h6>{job.salary}</h6>
-                          <p className="card-text">{job.jobDescription}</p>
-                          <button className="btn" onClick={() => { window.open(job.url, "_blank"); }}>View Job</button>
-                          <input
-                            type="button"
-                            value={this.state.saveText}
-                            className="btn"
-                            id="saveBtn"
-                            onClick={(e) => {
-                              e.target.value = "Saved";
-                              this.saveJob(
-                                job.jobTitle,
-                                job.jobLocation,
-                                job.jobDescription,
-                                job.organizationName,
-                                job.salary,
-                                job.jobBoard,
-                                job.url
-                              );
-                            }}
-                          />
-                        </div>
-                      </div>
-                    );
-                  })
+                </div>
+              );
+            })
+          )}
+        </div>
+        <div className="jobInfoContainer">
+          {this.state.results.map((job, index) => {
+            const isJobSelected = this.state.selectedJobIndex === index;
+            return (
+              <React.Fragment key={index}>
+                {isJobSelected && (
+                  <div className="jobInfoBody">
+                    <h4 className="card-title">{job.jobTitle} - {job.jobBoard}</h4>
+                    <h5 className="card-title">{job.organizationName}</h5>
+                    <h6>{job.jobLocation}</h6>
+                    <h6>{job.salary}</h6>
+                    <p className="card-text">{job.jobDescription}</p>
+                    <button className="btn" onClick={() => { window.open(job.url, "_blank"); }}>View Job</button>
+                    <input
+                      type="button"
+                      value={this.state.saveText}
+                      className="btn"
+                      id="saveBtn"
+                      onClick={(e) => {
+                        e.target.value = "Saved";
+                        this.saveJob(
+                          job.jobTitle,
+                          job.jobLocation,
+                          job.jobDescription,
+                          job.organizationName,
+                          job.salary,
+                          job.jobBoard,
+                          job.url
+                        );
+                      }}
+                    />
+                  </div>
                 )}
-              </div>
-              <div className="jobInfoContainer">
-                {this.state.results.map((job, index) => {
-                  const isJobSelected = this.state.selectedJobIndex === index;
-                  return (
-                    <React.Fragment key={index}>
-                      {isJobSelected && (
-                        <div className="jobInfoBody">
-                          <h4>Job Information</h4>
-                          <p>This is a test message for the jobInfo function.</p>
-                          <h4>Job Information</h4>
-                          <p>This is a test message for the jobInfo function.</p>
-                          <h4>Job Information</h4>
-                          <p>This is a test message for the jobInfo function.</p>
-                          <h4>Job Information</h4>
-                          <p>This is a test message for the jobInfo function.</p>
-                        </div>
-                      )}
-                    </React.Fragment>
-                  );
-                })}
-              </div>
-            </div>
-          );
-    }
+              </React.Fragment>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default JobCard;
