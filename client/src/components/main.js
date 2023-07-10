f258d9f6cb302433f155b83a91f2cfa722b9fdf9
Fengnan Zhang <73869130+zhangf2atwit@users.noreply.github.com>
2023-07-10 13:11:26 -0400

    added background and linkedin logo in home page, fixed placeholder color in about page

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import '../css/main.css'
+import linkedinImg from '../images/linkedinLogo.jpg'
 
 
 class Main extends Component {
@@ -36,19 +37,19 @@ class Main extends Component {
                         <h2>Featured Resources</h2>
                         <div class="resource-container">
                             <div class="resource-item">
-                                <img src="..img..linkedin_pic" alt="Resource 1"></img>
+                                <img src={linkedinImg} />
                                 <h3>Linkedin</h3>
                                 <p>Connect with professionals, build your network, and showcase your career achievements on the world's largest professional networking platform.</p>
-                                <a href="#" class="resource-link">Read More</a>
+                                <a href="http://linkedin.com" target="_blank" class="resource-link">Read More</a>
                             </div>
                             <div class="resource-item">
-                                <img src="resource2.jpg" alt="Resource 2"></img>
+                                <img src={linkedinImg}/>
                                 <h3>InDeed</h3>
                                 <p>Discover millions of job opportunities, browse reviews, and easily apply with the leading job search engine.</p>
                                 <a href="#" class="resource-link">Read More</a>
                             </div>
                             <div class="resource-item">
-                                <img src="resource3.jpg" alt="Resource 3"></img>
+                                <img src={linkedinImg}/>
                                 <h3>USA Jobs</h3>
                                 <p>Explore government employment opportunities and contribute to public service in various federal agencies across the United States.</p>
                                 <a href="#" class="resource-link">Read More</a>
@@ -60,19 +61,19 @@ class Main extends Component {
                         <h2>More Resources</h2>
                         <div class="resource-container">
                             <div class="resource-item">
-                                <img src="resource4.jpg" alt="Resource 4"></img>
+                                <img src={linkedinImg}/>
                                 <h3>Tittle</h3>
                                 <p> description</p>
                                 <a href="#" class="resource-link">Read More</a>
                             </div>
                             <div class="resource-item">
-                                <img src="resource5.jpg" alt="Resource 5"></img>
+                                <img src={linkedinImg}/>
                                 <h3>Resource 5 Title</h3>
                                 <p>Resource 5 description goes here.</p>
                                 <a href="#" class="resource-link">Read More</a>
                             </div>
                             <div class="resource-item">
-                                <img src="resource6.jpg" alt="Resource 6"></img>
+                                <img src={linkedinImg}/>
                                 <h3>Resource 6 Title</h3>
                                 <p>Resource 6 description goes here.</p>
                                 <a href="#" class="resource-link">Read More</a>
